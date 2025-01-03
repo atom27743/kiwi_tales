0d67c0033483bd284f361f8d4938d99b07e414d2
jjinalee <jinalee1697@gmail.com>
2025-01-04 01:33:10 +0900

    Updated permissions

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -15,24 +15,6 @@ else
     echo "ENVIRONMENT VARIABLE FOR GOOGLE_SERVICE_INFO_PLIST WAS NOT FOUND."
 fi
 
-# Check and create GenerativeAI-Info.plist
-if [ -n "$GENERATIVE_AI_INFO_PLIST" ]; then
-    echo "ENVIRONMENT VARIABLE FOR GENERATIVE_AI_INFO_PLIST WAS FOUND."
-    echo "$GENERATIVE_AI_INFO_PLIST" > "$CI_PRIMARY_REPOSITORY_PATH"/"$CI_PRODUCT"/"$CI_PRODUCT"/GenerativeAI-Info.plist
-    echo "GenerativeAI-Info.plist was created in the project directory."
-else
-    echo "ENVIRONMENT VARIABLE FOR GENERATIVE_AI_INFO_PLIST WAS NOT FOUND."
-fi
-
-# Check and create Development.xcconfig
-if [ -n "$DEVELOPMENT_XCCONFIG" ]; then
-    echo "ENVIRONMENT VARIABLE FOR DEVELOPMENT_XCCONFIG WAS FOUND."
-    echo "$DEVELOPMENT_XCCONFIG" > "$CI_PRIMARY_REPOSITORY_PATH"/"$CI_PRODUCT"/"$CI_PRODUCT"/Development.xcconfig
-    echo "Development.xcconfig was created in the project directory."
-else
-    echo "ENVIRONMENT VARIABLE FOR DEVELOPMENT_XCCONFIG WAS NOT FOUND."
-fi
-
 # Exit with a success code
 exit 0
 
